6ffcd77c6d8ba1a148e95c5fae4aa24a86d5f3b3
CodeWithNtebo <ST10485262@rcconnect.edu.za>
2025-08-19 08:34:32 +0000

    Initial commit

---
+++ src/whileloop/WhileLoop.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package whileloop;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class WhileLoop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // program state changed by user during run time
+        int state = 0;
+        int counter = 1;
+        while(state = 0){//begin loop
+            JOptionPane.showMessageDialog(parentComponent:null, "loop number: " + counter);
+            counter++;
+            
+            //local integer variable
+            int choice = Integer.parseInt(JOption.showinputDialog)
+        }
+    }
+    
+}
