3486668d81bd00e545dc25449989fc909f262b21
CodeWithNtebo <ST10485262@rcconnect.edu.za>
2025-08-19 08:35:24 +0000

    Finished WhileLoop

--- src/whileloop/WhileLoop.java
+++ src/whileloop/WhileLoop.java
@@ -10,22 +10,36 @@ import javax.swing.JOptionPane;
  *
  * @author RC_Student_lab
  */
+
 public class WhileLoop {
 
     /**
      * @param args the command line arguments
      */
+    
     public static void main(String[] args) {
         // program state changed by user during run time
         int state = 0;
         int counter = 1;
-        while(state = 0){//begin loop
-            JOptionPane.showMessageDialog(parentComponent:null, "loop number: " + counter);
+        while (state == 0){//begin loop
+            JOptionPane.showMessageDialog(null, "loop number:" + counter);
             counter++;
             
             //local integer variable
-            int choice = Integer.parseInt(JOption.showinputDialog)
-        }
-    }
+            int choice = Integer.parseInt(JOptionPane.showInputDialog("Do you want to continue? \n 1=No, 0 =Yes"));
+            switch(choice){
+                case'0':
+                    break;
+                    case'1':
+                        JOptionPane.showMessageDialog( null, "User terminated the program");
+                        state = choice;
+                        break;
+                    default:
+                        JOptionPane.showMessageDialog(null, "Wrong input" );
+                        
+                        
+            }//end of switch
+        }//end of whileloop
+    }//end of main
     
 }
